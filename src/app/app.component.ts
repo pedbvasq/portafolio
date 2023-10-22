3430b23f998dae1afacb1a3e8f4bffa02065f956
pedbvasq <pedbvasq@espol.edu.ec>
2023-10-22 12:09:36 -0500

    matriz 10x10

---
+++ src/app/app.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'portafolio';
+  celdas: { imagen: string; parrafo: string }[][];
+
+  constructor() {
+    this.celdas = Array.from({ length: 10 }, () => Array(10).fill({ imagen: '', parrafo: '' }));
+
+    // Asigna valores diferentes para cada celda
+    this.celdas[0][0] = { imagen: "../assets/pedro/EscapeDecarcel.png", parrafo: 'Es un juego 2d donde un ladron tiene que escapar de los guardias para hacerlo no tiene que ser visto, puede trepar paredes o esconderse, los guardias tienen linternas si lo notan lo llevaran a la carcel de nuevo' };
+    this.celdas[0][1] = { imagen: 'imagen2.jpg', parrafo:'Texto2' };
+    this.celdas[1][0] = { imagen: 'imagen3.jpg', parrafo: 'Texto 3' };
+    // y así sucesivamente...
+  }
+}
+
